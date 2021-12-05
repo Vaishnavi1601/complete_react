738ca69226ec1e24cc7f6c8539ee63d65ac1bb7a
Vaishnavi1601 <vaishnavi7734@gmail.com>
2021-12-05 14:05:24 +0530

    working with dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,12 +3,17 @@
 import "./ExpenseItem.css"; 
 
 function ExpenseItem() {
+
+  const expenseDate = new Date(2021, 12, 5);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
     <div className = "expense-item">
-      <div>December 5 , 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className = "expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className = "expense-item__price">$28.54</div>
+        <h2>{expenseTitle}</h2>
+        <div className = "expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
