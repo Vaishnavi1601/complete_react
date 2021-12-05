71c622dde444b68594c7e556cda62197b08009bb
Vaishnavi1601 <vaishnavi7734@gmail.com>
2021-12-05 14:20:50 +0530

    passing data via props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 788.21, date: new Date(2021, 3, 21) },
+    { title: "Bike Insurance", amount: 788.21, date: new Date(2021, 3, 21) },
+    { title: "Cycle Insurance", amount: 788.21, date: new Date(2021, 3, 21) },
+    { title: "Plain Insurance", amount: 788.21, date: new Date(2021, 3, 21) },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+        <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+        <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+         <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
-} 
+}
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,15 @@
 //component in js is a just a function, which returns JSX code.
-
+//props -- its an object that will holds all the received attributes as properties
 import "./ExpenseItem.css"; 
 
-function ExpenseItem() {
-
-  const expenseDate = new Date(2021, 12, 5);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+function ExpenseItem(props) { 
 
   return (
     <div className = "expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className = "expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className = "expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className = "expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
