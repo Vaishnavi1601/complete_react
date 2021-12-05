06f91e8db751006e612c031d79e438973d0025b9
Vaishnavi1601 <vaishnavi7734@gmail.com>
2021-12-05 18:33:57 +0530

    children props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,32 +1,18 @@
 //component in js is a just a function, which returns JSX code.
 //props -- its an object that will holds all the received attributes as properties
 import "./ExpenseItem.css"; 
-
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 function ExpenseItem(props) { 
 
-  const month = props.date.toLocaleString('en-US', {month: 'long'} )
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString('en-US', {day: '2-digit'}   )
-
-
-
   return (
-    <div className = "expense-item">
-      <div>
-
-      <div> {month} </div>
-
-      <div>{year}</div>
-
-      <div>{day}</div>
-
-      </div>
+    <Card className = "expense-item">
+      <ExpenseDate date= {props.date}/>
       <div className = "expense-item__description">
         <h2>{props.title}</h2>
         <div className = "expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 } 
-
 export default ExpenseItem;
