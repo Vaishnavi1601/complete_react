24e750a230640c12c8f344dd8e38210d00524599
Vaishnavi1601 <vaishnavi7734@gmail.com>
2021-12-05 12:28:46 +0530

    adding basic CSS

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,17 @@
 //component in js is a just a function, which returns JSX code.
 
+import "./ExpenseItem.css"; 
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className = "expense-item">
       <div>December 5 , 2021</div>
-      <div>
+      <div className = "expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$28.54</div>
+        <div className = "expense-item__price">$28.54</div>
       </div>
     </div>
   );
-}
+} 
 
 export default ExpenseItem;
