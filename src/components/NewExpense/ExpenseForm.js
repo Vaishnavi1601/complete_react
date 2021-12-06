4afd4c89d6f72fbef6199beee5756466aeee5a81
Vaishnavi1601 <vaishnavi7734@gmail.com>
2021-12-06 23:19:55 +0530

    two-way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,42 +2,74 @@ import React from "react";
 import { useState } from "react/cjs/react.development";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-
- const [enteredTitle, setEnteredTitle] =  useState(' ');
- const [enteredAmount, setEnteredAmount] = useState('');
- const [enteredDate, setEnteredDate] = useState('');
- const titleChangeHandler = (event) =>{
+  const [enteredTitle, setEnteredTitle] = useState(" ");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const titleChangeHandler = (event) => {
     // console.log(event);//event have target property
     //target contains current entered value
     // so to access current value --- event.target.value
     // console.log(event.target.value);
     setEnteredTitle(event.target.value);
-  }
+  };
 
-  const amountChangeHandler = (event) =>{
-      setEnteredAmount(event.target.value)
-  }
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value)
-  }
+    setEnteredDate(event.target.value);
+  };
 
+  const submitChangeHandler = (event) => {
+    event.preventDefault();
+    console.log(event);
 
-  return ( 
+    const expenseDate = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: enteredDate,
+    };
+    console.log(expenseDate);
+    //sets the state to empty string(our initial state) after submitting 
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
     <div>
-      <form>
-        <div className="n ew-expense__controls">
+      <form onSubmit={submitChangeHandler}>
+        <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" onChange = {titleChangeHandler} />
+            <input
+              type="text"
+              value={enteredTitle}
+              //two way binding -- for inputs we just dont listen to changes but we can also pass new value into input
+              //so that we can reset and change the input progr  amatically
+              onChange={titleChangeHandler}
+            />
           </div>
           <div className="new-expense__control label">
             <label>Amount</label>
-            <input type="number" onChange = {amountChangeHandler} min="0.01" step="0.01" />
+            <input
+              type="number"
+              value={enteredAmount}
+              onChange={amountChangeHandler}
+              min="0.01"
+              step="0.01"
+            />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" onChange = {dateChangeHandler} min="2018-01-01" max="2022-12-31" />
+            <input
+              type="date"
+              value={enteredDate}
+              onChange={dateChangeHandler}
+              min="2018-01-01"
+              max="2022-12-31"
+            />
           </div>
         </div>
         <div className="new-expense__actions">
